2190575a8586197b380f606c4a2b8a7650d0a7ff
Kevin Vu <kevinvud@gmail.com>
2019-03-24 21:24:16 -0700

    Added face eye and smile detection

---
+++ cv_superdata/face_eye_smile_detection.py
@@ -0,0 +1,35 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier('haarcascade_modules/haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_modules/haarcascade_eye.xml')
+smile_cascade = cv2.CascadeClassifier('haarcascade_modules/haarcascade_smile.xml')
+
+def detect(gray_image, original_image):
+    faces = face_cascade.detectMultiScale(gray_image, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(original_image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        roi_gray = gray_image[y:y+h, x:x+w]
+        roi_color = original_image[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 22)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey + eh), (0, 255, 0), 2)
+        smile = smile_cascade.detectMultiScale(roi_gray, 1.1, 22)
+        for (sx, sy, sw, sh) in smile:
+            cv2.rectangle(roi_color, (sx, sy), (sx+sw, sy + sh), (0, 0, 255), 2)
+    return original_image
+
+video_capture = cv2.VideoCapture(0)
+
+# Check if camera opened successfully
+if not video_capture.isOpened():
+    print("Unable to read camera feed")
+else:
+    while True:
+        _, video_feed = video_capture.read()
+        feedToGray = cv2.cvtColor(video_feed, cv2.COLOR_BGR2GRAY)
+        canvas = detect(feedToGray, video_feed)
+        cv2.imshow('Video', canvas)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    video_capture.release()
+    cv2.destroyAllWindows()
